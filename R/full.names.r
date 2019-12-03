7ff62a4de519cb08d4717469f0bee7236d2d1972
Tim Bergsma <bergsmat@gmail.com>
2019-12-03 14:29:16 -0500

    merge-issue01

--- R/full.names.r
+++ R/full.names.r
@@ -14,7 +14,12 @@
 full.names = function(x)
 {
   swap(x, 
-                  Cs(wt,ht,bmi,age,sex,crcl,race),
+                  c('wt','ht','bmi','age','sex','crcl','race'),
                   c("Weight (kg)","Height (cm)","BMI (kg/m2)","Age (yr)","Gender","Creatinine Clearance (mL/min)","Race")
   )      
+}
+
+if(F)
+{
+  full.names(c("wt","race","age"))
 }
