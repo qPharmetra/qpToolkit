116dc58fc42e27112844944e7e87ac7b75b5fca9
Tim Bergsma <bergsmat@gmail.com>
2019-12-03 12:48:33 -0500

    prettyprint original and suggestion

--- R/ggvpc_xpose.R
+++ R/ggvpc_xpose.R
@@ -51,59 +51,100 @@
 #'   scale_y_log10() + scale_x_log10() + facet_grid(~strata)
 #' 
 
-ggvpc_xpose = function(vpc
-                       , PI = c(0.025,0.975)
-                       , area.col.central = PI.ci.med.arcol
-                       , area.col.outer = gray(0.2)
-                       , linecol.pred = "blue"
-                       , linesize.pred = 1
-                       , linetype.obs = "dashed"
-                       , linecol.obs = "black"
-                       , linesize.obs = 0.5
-                       , alpha = 0.33
-                       , point.shape = 1
-                       , point.size = 1.25
-                       , point.col = "darkslategrey"
-                       , yrange.stretch = c(0.9,1.1)
-                       , quiet = TRUE
-)
+ggvpc_xpose = function(vpc,
+                       PI = c(0.025, 0.975),
+                       area.col.central = PI.ci.med.arcol,
+                       area.col.outer = gray(0.2),
+                       linecol.pred = 'blue',
+                       linesize.pred = 1,
+                       linetype.obs = 'dashed',
+                       linecol.obs = 'black',
+                       linesize.obs = 0.5,
+                       alpha = 0.33,
+                       point.shape = 1,
+                       point.size = 1.25,
+                       point.col = 'darkslategrey',
+                       yrange.stretch = c(0.9, 1.1),
+                       quiet = TRUE)
 {
-  if(!quiet) message("preparing plot ...")
+  if (!quiet)
+    message('preparing plot ...')
   
   #require(ggplot2)
-  names(vpc$obs)[names(vpc$obs)==unique(vpc$obs$idv.var)]="IVAR"
-  names(vpc$obs)[names(vpc$obs)==unique(vpc$obs$dv.var)]="DVVAR"
+  names(vpc$obs)[names(vpc$obs) == unique(vpc$obs$idv.var)] = 'IVAR'
+  names(vpc$obs)[names(vpc$obs) == unique(vpc$obs$dv.var)] = 'DVVAR'
   
-  PI = PI*100
+  PI = PI * 100
   
-  cin = names(vpc$vpc)[grepl("CI.for", names(vpc$vpc))][1]
-  cin = sub("vpc","", unPaste(cin, sep="[.]")[[1]])
+  cin = names(vpc$vpc)[grepl('CI.for', names(vpc$vpc))][1]
+  cin = sub('vpc', '', unPaste(cin, sep = '[.]')[[1]])
   
-  vpc$res$piLowerDown   = vpc$res[, paste0("vpc",cin,".CI.for.",PI[1],".from")]
-  vpc$res$piLowerUp     = vpc$res[, paste0("vpc",cin,".CI.for.",PI[1],".to")]
-  vpc$res$piUpperDown   = vpc$res[, paste0("vpc",cin,".CI.for.",PI[2],".from")]
-  vpc$res$piUpperUp     = vpc$res[, paste0("vpc",cin,".CI.for.",PI[2],".to")]
-  vpc$res$piCentralDown = vpc$res[, paste0("vpc",cin,".CI.for.50.from")]
-  vpc$res$piCentralUp   = vpc$res[, paste0("vpc",cin,".CI.for.50.to")]
+  vpc$res$piLowerDown   = vpc$res[, paste0('vpc', cin, '.CI.for.', PI[1], '.from')]
+  vpc$res$piLowerUp     = vpc$res[, paste0('vpc', cin, '.CI.for.', PI[1], '.to')]
+  vpc$res$piUpperDown   = vpc$res[, paste0('vpc', cin, '.CI.for.', PI[2], '.from')]
+  vpc$res$piUpperUp     = vpc$res[, paste0('vpc', cin, '.CI.for.', PI[2], '.to')]
+  vpc$res$piCentralDown = vpc$res[, paste0('vpc', cin, '.CI.for.50.from')]
+  vpc$res$piCentralUp   = vpc$res[, paste0('vpc', cin, '.CI.for.50.to')]
   
-  vpc$vpc$obsLower = vpc$vpc[, paste0("vpc",PI[1],".real")]
-  vpc$vpc$obsUpper = vpc$vpc[, paste0("vpc",PI[2],".real")]
+  vpc$vpc$obsLower = vpc$vpc[, paste0('vpc', PI[1], '.real')]
+  vpc$vpc$obsUpper = vpc$vpc[, paste0('vpc', PI[2], '.real')]
   
   p = ggplot() + theme_bw() +
-    geom_ribbon(data = vpc$res, aes(x=xCov,ymin=piLowerDown,ymax=piLowerUp)
-                , fill=area.col.outer,alpha=alpha) +
-    geom_ribbon(data = vpc$res, aes(x=xCov,ymin=piUpperDown,ymax=piUpperUp)
-                , fill=area.col.outer,alpha=alpha) +
-    geom_ribbon(data = vpc$res, aes(x=xCov,ymin=piCentralDown,ymax=piCentralUp)
-                , fill=area.col.central,alpha=alpha) +
+    geom_ribbon(
+      data = vpc$res,
+      aes(x = xCov, ymin = piLowerDown, ymax = piLowerUp)
+      ,
+      fill = area.col.outer,
+      alpha = alpha
+    ) +
+    geom_ribbon(
+      data = vpc$res,
+      aes(x = xCov, ymin = piUpperDown, ymax = piUpperUp)
+      ,
+      fill = area.col.outer,
+      alpha = alpha
+    ) +
+    geom_ribbon(
+      data = vpc$res,
+      aes(x = xCov, ymin = piCentralDown, ymax = piCentralUp)
+      ,
+      fill = area.col.central,
+      alpha = alpha
+    ) +
     
-    geom_line(data = vpc$vpc, aes(x=xCovm,y=obsLower),linetype=linetype.obs, size=linesize.obs) +
-    geom_line(data = vpc$vpc, aes(x=xCovm,y=vpc50.real),linetype=linetype.obs, size=linesize.obs) +
-    geom_line(data = vpc$vpc, aes(x=xCovm,y=obsUpper),linetype=linetype.obs, size=linesize.obs) +
+    geom_line(
+      data = vpc$vpc,
+      aes(x = xCovm, y = obsLower),
+      linetype = linetype.obs,
+      size = linesize.obs
+    ) +
+    geom_line(
+      data = vpc$vpc,
+      aes(x = xCovm, y = vpc50.real),
+      linetype = linetype.obs,
+      size = linesize.obs
+    ) +
+    geom_line(
+      data = vpc$vpc,
+      aes(x = xCovm, y = obsUpper),
+      linetype = linetype.obs,
+      size = linesize.obs
+    ) +
     coord_cartesian(ylim = yrange.stretch * range(vpc$obs$DV)) +
-    geom_line(data = vpc$vpc, aes(x=xCovm, y=vpc50.sim), color=linecol.pred, alpha=0.75, size=linesize.pred) +
-    geom_point(data=vpc$obs, aes(x=IVAR,y=DVVAR), col = point.col, shape = point.shape, size = point.size) 
-   
+    geom_line(
+      data = vpc$vpc,
+      aes(x = xCovm, y = vpc50.sim),
+      color = linecol.pred,
+      alpha = 0.75,
+      size = linesize.pred
+    ) +
+    geom_point(
+      data = vpc$obs,
+      aes(x = IVAR, y = DVVAR),
+      col = point.col,
+      shape = point.shape,
+      size = point.size
+    )
+  
   p
 }
-
